dd3583a74ac02fa98d27aee28d68b1603f1798e1
BryanAlexisMartinez <b.alexis.mtz.a@gmail.com>
2023-08-10 15:07:01 -0600

    correcion de codigo

--- formulario.js
+++ formulario.js
@@ -1,8 +1,9 @@
-var formulario = document.querySelector("#form")
+var formulario = document.querySelector(".formulario")
+var listaInvitados = document.getElementById("lista-de-invitados");
 
 formulario.onsubmit = function(e) {
 
-  e.prevent();
+  e.preventDefault();
   
   var n = formulario.elements[0]
   var e = formulario.elements[1]
@@ -17,25 +18,31 @@ formulario.onsubmit = function(e) {
   console.log(nacionalidad)
 
   if (nombre.length === 0) {
-    n.classList.add("error")
-  }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+    n.classList.add("error");
+  } else {
+    n.classList.remove("error"); 
   }
 
+  if (edad < 18 || edad > 120) {
+    e.classList.add("error");
+  } else {
+    e.classList.remove("error"); // Quitamos la clase de error si se proporciona una edad válida
+  }
+
+
 if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
+  && (edad >= 18 
+    && edad <= 100) ) {
   agregarInvitado(nombre, edad, nacionalidad)
   }
 }
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
+//var botonBorrar = document.createElement("button")
+//botonBorrar.textContent = "Eliminar invitado"
+//botonBorrar.id = "boton-borrar"
 var corteLinea = document.createElement("br")
 document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
+//document.body.appendChild(botonBorrar);
 
 function agregarInvitado(nombre, edad, nacionalidad) {
 
@@ -55,7 +62,7 @@ function agregarInvitado(nombre, edad, nacionalidad) {
 var lista = document.getElementById("lista-de-invitados")
 
 var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
+elementoLista.classList.add("elemento-lista")
 lista.appendChild(elementoLista)
 
 var spanNombre = document.createElement("span")
@@ -63,8 +70,8 @@ var inputNombre = document.createElement("input")
 var espacio = document.createElement("br")
 spanNombre.textContent = "Nombre: "
 inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
+//elementoLista.appendChild(spanNombre)
+//elementoLista.appendChild(inputNombre)
 elementoLista.appendChild(espacio)
 
 function crearElemento(descripcion, valor) {
@@ -95,3 +102,36 @@ elementoLista.appendChild(botonBorrar);
 botonBorrar.parentNode.remove()
   }
 }
+
+/*
+Se comentaron los elementos de la linea  34,35,36,39 ya que estos hacian que se viera un boton de eliminar invitado el cual no funcionaba y
+solo estaba de adorno,
+se var listaInvitados = document.getElementById("lista-de-invitados"); trajo el elemento de liste-de-invitados ya que no estaba declarado
+en el html se puso el id en:
+ <div id="lista-de-invitados">
+    <h2>Lista de invitados</h2>
+  </div>
+  para poder mostrar ahi la lista
+  se ha quitado un punto en la declaracion del script
+
+  se puso como comentario:
+  //elementoLista.appendChild(spanNombre)
+//elementoLista.appendChild(inputNombre)
+ya que mostraba doble vez el nombre del invitado
+
+// Quita la clase de error si se proporciona un nombre correcto
+ if (nombre.length === 0) {
+    n.classList.add("error");
+  } else {
+    n.classList.remove("error"); 
+  }
+
+
+    if (edad < 18 || edad > 120) {
+    e.classList.add("error");
+  } else {
+    e.classList.remove("error"); // Quita la clase de error si se proporciona una edad valida
+  }
+
+
+*/
